abf5e941c26cc490564ea60a6af078c7d9319c8a
Levsnol <124164978+Levsnol@users.noreply.github.com>
2024-04-20 21:30:55 -0500

    Update UNHITTABLE.lua

--- UNHITTABLE.lua
+++ UNHITTABLE.lua
@@ -1,7 +1,7 @@
 getgenv()._=
 "This script made by xk5ng | 808188341547565097 | If you pay for this script you get scammed LMAO | This script is completely free ok | Please kill yourself if you sell this script | On god skibidi rizzler meow gyat ohio sigma | https://discord.gg/msgabv2t9Q | https://discord.gg/stando"
 --// ! Accounts ! \\--
-getgenv().Accounts = {OWNER = 'ttiiiiiiiixtttxiitix', STAND = "wookington"}
+getgenv().Accounts = {OWNER = 'ttiiiiiiiixtttxiitix', STAND = "wookinton"}
 --\\-- Stand-Settings --//--
 getgenv().StandConfig = {
     --\\-- ! Main ! --//--
